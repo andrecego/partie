631be44b553e4424e92fa63a9ce168c39b9d9b21
Andre Cesar Gomes <andre.cesgo@gmail.com>
2022-07-10 18:11:00 -0300

    chore: move discord token to env var

--- config/main.go
+++ config/main.go
@@ -1,50 +1,15 @@
 package config
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-)
+import "os"
 
 var (
 	Token     string //To store value of Token from config.json .
 	BotPrefix string // To store value of BotPrefix from config.json.
-
-	config *configStruct //To store value extracted from config.json.
 )
 
-type configStruct struct {
-	Token     string `json:"token"`
-	BotPrefix string `json:"botPrefix"`
-}
-
 func ReadConfig() error {
-	fmt.Println("Reading config file...")
-	file, err := ioutil.ReadFile("./config.json") // ioutil package's ReadFile method which we read config.json and return it's value we will then store it in file variable and if an error ocurrs it will be stored in err .
+	Token = os.Getenv("TOKEN")
+	BotPrefix = "!"
 
-	//Handling error and printing it using fmt package's Println function and returning it .
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	// We are here printing value of file variable by explicitly converting it to string .
-
-	fmt.Println(string(file))
-	// Here we performing a simple task by copying value of file into config variable which we have declared above , and if there any error we are storing it in err . Unmarshal takes second arguments reference remember it .
-	err = json.Unmarshal(file, &config)
-
-	//Handling error
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	// After storing value in config variable we will access it and storing it in our declared variables .
-	Token = config.Token
-	BotPrefix = config.BotPrefix
-
-	//If there isn't any error we will return nil.
 	return nil
-
 }
